83c0592b9a1cf4aa05d5758b3eacc547a96049fb
Travis Barney <travis@needle.com>
2015-09-20 01:43:39 -0600

    Base gray sphere, ready to solve drag/spin logic.

--- src/components/orb/eco/view.js
+++ src/components/orb/eco/view.js
@@ -11,20 +11,19 @@ function renderCaption() {
 function renderEco() {
 
   /**
-   * 
-   *
-   *
+   * Generate the markup and plot sphere-face positions.
    */
   let pyra = function(rows = 22, scale = 2, color = "#F22") {
     const RAD = rows * scale;
     const ROWS = rows + 1;
-    const LEN = (Math.PI / 2) / ROWS;
 
-    let angX = (90 / ROWS) ;
+    let angX = (90 / (ROWS - 0.5));
     let rotX = 0, rotY = 0, rotZ = 0;
     let step = 1;
 
-    return _.times(ROWS + 1, (row) => {
+    color = 'rgba(0,0,0, 0.3)';
+
+    return _.times(ROWS, (row) => {
       row += 1;
       let angY = 0; // Not sure if needed.
       let angZ = 60 / row;
